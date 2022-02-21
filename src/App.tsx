7c18b52ee9a645c9cb96a03b41e6cc6498abc5d3
Ian Propper <ian@dropps.com>
2022-02-21 16:11:22 -0500

    Added Packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "@emotion/styled";
+
+const BlueBox = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: lightblue;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <BlueBox>blue</BlueBox>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
